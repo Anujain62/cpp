2676d3cb0a08ac094a272c2f4acd2614e13b63fc
Anu Jain <anujain.aj021@gmail.com>
2025-05-12 11:06:29 +0530

    Add files via upload

---
+++ HollowRectangle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"enter number of rows & coloums:";
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        if(i==0 || i==n-1){
+            for(int j=0;j<m;j++)
+                cout<<"* ";
+        }
+        else{
+            cout<<"* ";
+            for(int j=0;j<m-2;j++)
+                cout<<"  ";
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
