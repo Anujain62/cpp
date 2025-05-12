d9212b65d15595fc81c124312538f1b8976665f0
Anu Jain <anujain.aj021@gmail.com>
2025-05-12 11:07:12 +0530

    Add files via upload

---
+++ HollowSquare.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        if(i==0 || i==n-1){
+            for(int j=0;j<n;j++)
+                cout<<"* ";
+        }
+        else{
+            cout<<"* ";
+            for(int j=0;j<n-2;j++)
+                cout<<"  ";
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
