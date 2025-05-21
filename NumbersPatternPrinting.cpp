7011aa424e5c23867c41d73fca612398934c3296
Anu Jain <anujain.aj021@gmail.com>
2025-05-21 22:52:08 +0530

    Add files via upload

---
+++ NumbersPatternPrinting.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+
+
+/**       1
+         121
+        12321
+       1234321
+**/
+/*int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>i;j--)
+            cout<<" ";
+        for(int j=1;j<=i;j++)
+            cout<<j;
+        for(int k=i-1;k>=1;k--)
+            cout<<k;
+        cout<<endl;
+    }
+}*/
+
+
+/**       1
+         12
+        123
+       1234
+ **/
+ /*int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>i;j--)
+            cout<<" ";
+        for(int j=1;j<=i;j++)
+            cout<<j;
+        cout<<endl;
+    }
+ }*/
+
+
+ /**     1
+         22
+         333
+         4444
+ **/
+ /*int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)
+            cout<<i;
+        cout<<endl;
+    }
+ }*/
+
+ /**   1
+       12
+       123
+       1234
+ **/
+ int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++)
+            cout<<j;
+        cout<<endl;
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
