b54d23c06efae40bfe25e6e180bf8c8b026508fc
Anu Jain <anujain.aj021@gmail.com>
2025-05-25 19:44:10 +0530

    Add files via upload

---
+++ CalculateSumOf1ToNSeries.cpp
@@ -0,0 +1,12 @@
+/** 1+2+3+4+5+6+......**/
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        sum+=i;
+    cout<<sum;
+    return 0;
+}
