40ca25ae68cbfc677e5e6890e25310d6ba981ed2
Anu Jain <anujain.aj021@gmail.com>
2025-06-03 21:53:49 +0530

    Add files via upload

---
+++ CreateEnrollNumberUsingStudentClass.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class student{
+    string name;
+    string branch;
+    string clgname;
+    int yearofAdd;
+    bool preQuali;
+
+};
+
+int main(){
+    return 0;
+}
